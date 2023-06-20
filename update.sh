fe76e1dc445171cebc5b6d1e5ee2476a7f535d0d
hos-os <86213274+HOS-OS@users.noreply.github.com>
2023-06-19 23:32:51 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ clear
 echo "test for hos update second time"
 echo " lol i cant beleave this worked"
 sudo apt update
-sudo apt install -y geany
+sudo apt install -y xpad
 clear
 echo "why am i here"
 clear
