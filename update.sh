bd213e382a997cbd5ec30ecedf1fafc87969e7c8
hos-os <86213274+HOS-OS@users.noreply.github.com>
2023-06-20 21:52:56 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ clear
 echo "test for hos update second time"
 echo " lol i cant beleave this worked"
 sudo apt update
-sudo apt remove --purge -y xpad
+sudo apt remove --purge -y vlc
 clear
 echo "why am i here"
 clear
